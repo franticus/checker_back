bb11e054dc01d5cfec2c0a8a67f71d22801fc395
S.Zelensky <zelenstein@gmail.com>
2024-01-31 19:31:33 +0300

    v2.1.0

--- commands/test.js
+++ commands/test.js
@@ -3,6 +3,115 @@ const path = require('path');
 const cheerio = require('cheerio');
 const AdmZip = require('adm-zip');
 
+async function processHtmlFile(
+  filename,
+  content,
+  results,
+  titles,
+  descriptions
+) {
+  const $ = cheerio.load(content);
+  const forms = $('form');
+  let formCheck = true;
+
+  forms.each((i, form) => {
+    if ($(form).find('input[type="checkbox"]').length === 0) {
+      formCheck = false;
+      results.push(`Форма без чекбокса найдена в файле: ${filename}`);
+    }
+  });
+
+  $('form').each((i, form) => {
+    const action = $(form).attr('action');
+    if (!action || !action.endsWith('.php')) {
+      results.push(
+        `Отсутствует или некорректный атрибут action у формы в файле: ${filename}`
+      );
+    }
+
+    $(form)
+      .find('input:not([type="submit"])')
+      .each((j, input) => {
+        const inputElement = $(input);
+        const hasRequired =
+          inputElement.attr('required') === '' ||
+          inputElement.attr('required') === 'required';
+
+        if (!hasRequired) {
+          results.push(
+            `Отсутствует атрибут required у input в файле: ${filename}`
+          );
+        }
+
+        const hasName = inputElement.attr('name') !== undefined;
+
+        if (!hasName) {
+          results.push(`Отсутствует атрибут name у input в файле: ${filename}`);
+        }
+      });
+
+    const textareaStyle = $(form).find('textarea').attr('style');
+    if (textareaStyle && !textareaStyle.includes('resize: none')) {
+      results.push(
+        `У textarea отсутствует свойство resize: none в файле: ${filename}`
+      );
+    }
+  });
+
+  const title = $('title').text();
+  const description = $('meta[name="description"]').attr('content');
+
+  if (titles.has(title) || descriptions.has(description)) {
+    results.push(`Дублированные мета теги найдены в файле: ${filename}`);
+  } else {
+    titles.add(title);
+    descriptions.add(description);
+  }
+
+  if (filename.endsWith('.html')) {
+    const links = $('a');
+
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      const href = $(link).attr('href');
+      if (href) {
+        if (
+          href === '' ||
+          href.startsWith('#') ||
+          href.endsWith('.html') ||
+          href.includes('#')
+        ) {
+          continue;
+        } else if (
+          !href.includes('http') &&
+          !href.includes('tel:') &&
+          !href.includes('mailto')
+        ) {
+          results.push(
+            `Недопустимая ссылка (href: ${href}) обнаружена в файле: ${filename}`
+          );
+        }
+      }
+    }
+  }
+
+  if (filename.endsWith('.html')) {
+    const links = $('a[href^="#"]');
+    links.each((i, link) => {
+      const href = $(link).attr('href');
+      if (href.length > 1) {
+        const anchorId = href.substring(1);
+        const target = $(`#${anchorId}`);
+        if (target.length === 0) {
+          results.push(
+            `Отсутствует якорь для ссылки ${href} в файле: ${filename}`
+          );
+        }
+      }
+    });
+  }
+}
+
 async function checkHtmlFiles(directory) {
   const titles = new Set();
   const descriptions = new Set();
@@ -10,111 +119,7 @@ async function checkHtmlFiles(directory) {
 
   async function processFile(filename, content) {
     if (filename.endsWith('.html')) {
-      const $ = cheerio.load(content);
-      const forms = $('form');
-      let formCheck = true;
-
-      forms.each((i, form) => {
-        if ($(form).find('input[type="checkbox"]').length === 0) {
-          formCheck = false;
-          results.push(`Форма без чекбокса найдена в файле: ${filename}`);
-        }
-      });
-
-      $('form').each((i, form) => {
-        const action = $(form).attr('action');
-        if (!action || !action.endsWith('.php')) {
-          results.push(
-            `Отсутствует или некорректный атрибут action у формы в файле: ${filename}`
-          );
-        }
-
-        $(form)
-          .find('input:not([type="submit"])')
-          .each((j, input) => {
-            const inputElement = $(input);
-            const hasRequired =
-              inputElement.attr('required') === '' ||
-              inputElement.attr('required') === 'required';
-
-            if (!hasRequired) {
-              results.push(
-                `Отсутствует атрибут required у input в файле: ${filename}`
-              );
-            }
-
-            const hasName = inputElement.attr('name') !== undefined;
-
-            if (!hasName) {
-              results.push(
-                `Отсутствует атрибут name у input в файле: ${filename}`
-              );
-            }
-          });
-
-        const textareaStyle = $(form).find('textarea').attr('style');
-        if (textareaStyle && !textareaStyle.includes('resize: none')) {
-          results.push(
-            `У textarea отсутствует свойство resize: none в файле: ${filename}`
-          );
-        }
-      });
-
-      const title = $('title').text();
-      const description = $('meta[name="description"]').attr('content');
-
-      if (titles.has(title) || descriptions.has(description)) {
-        results.push(`Дублированные мета теги найдены в файле: ${filename}`);
-      } else {
-        titles.add(title);
-        descriptions.add(description);
-      }
-
-      if (filename.endsWith('.html')) {
-        const $ = cheerio.load(content);
-        const links = $('a');
-
-        for (let i = 0; i < links.length; i++) {
-          const link = links[i];
-          const href = $(link).attr('href');
-          if (href) {
-            if (
-              href === '' ||
-              href.startsWith('#') ||
-              href.endsWith('.html') ||
-              href.includes('#')
-            ) {
-              continue;
-            } else if (
-              !href.includes('http') &&
-              !href.includes('tel:') &&
-              !href.includes('mailto')
-            ) {
-              results.push(
-                `Недопустимая ссылка (href: ${href}) обнаружена в файле: ${filename}`
-              );
-            }
-          }
-        }
-      }
-
-      if (filename.endsWith('.html')) {
-        const $ = cheerio.load(content);
-
-        const links = $('a[href^="#"]');
-        links.each((i, link) => {
-          const href = $(link).attr('href');
-          if (href.length > 1) {
-            const anchorId = href.substring(1);
-            const target = $(`#${anchorId}`);
-            if (target.length === 0) {
-              results.push(
-                `Отсутствует якорь для ссылки ${href} в файле: ${filename}`
-              );
-            }
-          }
-        });
-      }
+      await processHtmlFile(filename, content, results, titles, descriptions);
     }
   }
 
