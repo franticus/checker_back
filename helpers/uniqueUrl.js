42ed64b8e5adff936cf1c4b9ff01d1daf226c88a
S.Zelensky <zelenstein@gmail.com>
2024-02-28 20:56:21 +0300

    URLdeploy

--- helpers/uniqueUrl.js
+++ helpers/uniqueUrl.js
@@ -1,46 +1,54 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
+const chromium = require('@sparticuz/chromium');
 const JSZip = require('jszip');
 const fs = require('fs');
 const path = require('path');
 
 async function downloadSitePagesAsZip(siteUrl) {
-  const browser = await puppeteer.launch();
+  // Асинхронно получаем путь к исполняемому файлу
+  const executablePath = await chromium.executablePath();
+  console.log('executablePath:', executablePath);
+
+  const browser = await puppeteer.launch({
+    executablePath, // Теперь здесь будет строка с путём
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+
   const page = await browser.newPage();
   await page.goto(siteUrl, { waitUntil: 'networkidle2' });
 
-  // Сбор ссылок на страницы сайта.
+  // Сбор ссылок на страницы сайта
   const urls = await page.evaluate(() => {
     return Array.from(document.querySelectorAll('a'))
       .map(link => link.href)
       .filter(href => href.startsWith(window.location.origin));
   });
 
-  // Уникальные URL для избежания дубликатов.
+  // Уникальные URL для избежания дубликатов
   const uniqueUrls = [...new Set(urls)];
 
   const zip = new JSZip();
   for (const url of uniqueUrls) {
     const pageContent = await downloadPageContent(url, browser);
-    // Извлечение имени страницы из URL и сохранение контента в ZIP.
+    // Извлечение имени страницы из URL и сохранение контента в ZIP
     const pageName = url.replace(/[^a-z0-9]/gi, '_').toLowerCase() + '.html';
     zip.file(pageName, pageContent);
   }
 
   await browser.close();
 
-  // Генерация имени для ZIP-файла на основе доменного имени сайта.
+  // Генерация имени для ZIP-файла на основе доменного имени сайта
   const siteDomain = new URL(siteUrl).hostname
     .replace(/^www\./, '')
-    .split('.')[0]; // Удаление www и взятие домена первого уровня
-  const zipName = `${siteDomain}.zip`; // Фиксированное имя файла
+    .split('.')[0];
+  const zipName = `${siteDomain}.zip`;
   const outputPath = path.join(__dirname, '..', 'uploads', zipName);
 
-  // Сохранение ZIP-архива.
+  // Сохранение ZIP-архива
   const zipContent = await zip.generateAsync({ type: 'nodebuffer' });
   fs.writeFileSync(outputPath, zipContent);
 
   console.log(`Страницы сайта сохранены в ${outputPath}`);
-  // Возвращаем путь к созданному ZIP-файлу.
   return outputPath;
 }
 
