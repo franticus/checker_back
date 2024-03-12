d40ab4a5c750362b3d321cfebeb11b1354e9ddb7
S.Zelensky <zelenstein@gmail.com>
2024-03-12 13:35:51 +0300

    cleanUploads

--- commands/cleanUploadsHandler.js
+++ commands/cleanUploadsHandler.js
@@ -1,14 +1,19 @@
 const fs = require('fs-extra');
 const path = require('path');
+
 const uploadsDir = path.join(__dirname, '..', 'uploads');
+const tempExtractedDir = path.join(__dirname, '..', 'temp_extracted');
 
 async function cleanUploads(req, res) {
   try {
     await fs.emptyDir(uploadsDir);
-    res.send('Uploads directory has been cleared successfully.');
+    await fs.remove(tempExtractedDir);
+    res.send(
+      'Uploads and temporary extraction directories have been cleared successfully.'
+    );
   } catch (error) {
-    console.error('Error clearing uploads directory:', error);
-    res.status(500).send('Error clearing uploads directory');
+    console.error('Error clearing directories:', error);
+    res.status(500).send('Error clearing directories');
   }
 }
 
